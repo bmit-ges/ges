e7ef75ab421dcee962fc0ef1cca88ba5ba12684a
ssiissii100 <sibylle.gehring@espas.ch>
2023-02-24 13:20:14 +0100

    new page

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="js/accordion.js"></script>
+
+    <title>ges.bm-it.ch</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <link rel="stylesheet" href="css/largescreen.css">
+    <link rel="stylesheet" href="css/mediumscreen.css">
+    <link rel="stylesheet" href="css/smalscreen.css">
+    <link rel="stylesheet" href="css/accordion.css">
+    <link rel="stylesheet" href="css/contactform.css">
+    <link rel="stylesheet" href="css/certificate.css">
+    <link rel="stylesheet" href="css/projects.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script type="text/javascript" scr="/js/accordion.js"></script>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">About Me</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Lebenslauf</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Projekte</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Zertifikate</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kontakt</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+    <content >
+        <div class="conten-container">
+            <div class="circle"><h1>Welcome<h1></div>
+            <div class = "welcome-animation-1"></div>
+            <div class = "welcome-animation-2"></div>
+            <div class = "welcome-animation-3"></div>
+            <div class = "welcome-animation-4"></div>
+            <div class = "welcome-animation-5"></div>
+            <div class = "welcome-animation-6"></div>
+        </div>
+        </div>
+
+        <div class="container-infos">
+            
+            <img class="myimg" src="img/profilbild.jpg" alt="profilbild">
+            <ul>
+                <li>Name: Sibylle Gehring</li>
+                <li>Nationalität: Schweizerin</li>
+                <li>Geburtsdatum: 18.12.1997</li>
+                <li>Wohnort: Zürich</li>
+                
+            </ul>
+
+            <p>
+                
+                Geboren und aufgewachsen bin ich in der Stadt Zürich, gemeinsam mit meinen
+                Eltern und meinem älteren Bruder. Schon früh habe ich Interesse an Technologie gezeigt.
+                Vor allem mein Vater hat mir dabei sehr vieles gezeigt und erklärt.
+                Aber auch die Begeisterung für die Kunst wurde schon früh bei mir geweckt,
+                diese kam vor allem von meinem Grossvater. Zu meinen Hobbies gehören daher Zeichnen,
+                Malen in traditioneller aber auch digitaler Form. Zudem agiere ich schon seit meiner Grundschulzeit
+                als erste Ansprechperson bei technischen Problemen welche Freunde, Familie aber auch damals
+                Lehrpersonen immer wieder hatten. Die Liebe zur Technik lebe ich somit nicht nur im Beruf,
+                sondern auch privat gerne aus, eine der grössten Freuden ist für mich,
+                nachdem ein Problem gelöst wurde die Person, die Hilfe gesucht hat, zufrieden 
+                und glücklich zu sehen.
+            </p>
+        </div>
+
+        <div class="container-accordion">
+            <div>
+                <h2 class="accordion-header">
+                    2020 - 2021
+                </h2>
+                <p class="accordion-body">
+                    Nach einer schwierigen Zeit raffte ich mich
+                    auf und fing bei der Firma ESPAS ein Aufbautraining an.
+                    Schon schnell waren Fortschritte zusehen, weswegen ich nach
+                    vier Monaten bereits die Berufsvorbereitung in der internen
+                    Informatikabteilung beginnen konnte.
+                </p>
+            </div>
+            <div>
+                <h2 class="accordion-header">
+                    2016 - 2018
+                </h2>
+                <p class="accordion-body">
+                    Um meinen Sekundarabschluss zu erweitern, habe ich
+                    mich schliesslich dazu entschieden, an der Atelierschule
+                    den IMS F Abschluss zu machen, dieser ist gleichwertig
+                    wie der Fachmittelschulabschluss. Spezialisiert habe ich mich
+                    dabei auf Mathematik, Chemie, Biochemie und Eurythmie.
+                </p>
+            </div>
+            <div>
+                <h2 class="accordion-header">
+                    2013 - 2014
+                </h2>
+                <p class="accordion-body">
+                    Das Berufvorbereitungsjahr absolvierte ich an der
+                    Fachschule Viventa. Ich entschied mich  damals für die
+                    Fachrichtung Gestaltung und Kunst. Neben den normalen Schulfächern
+                    besuchte ich ebenfalls den Informatikunterricht, Theater,
+                    bildnerisches Gestalten und dreidimensionales Gestalten
+                    als Unterrichtsfächer
+                </p>
+            </div>
+            <div>
+                <h2 class="accordion-header">
+                    2010 - 2013
+                </h2>
+                <p class="accordion-body">
+                    Während den drei Jahren Sekundarschule
+                    besuchte ich in Albisrieden das Schulhaus Letzi
+                    auf Sekundarniveau A mit der Einstufung 
+                    1 in Mathematik und 2 in Französisch.
+                </p>
+            </div>
+        </div>
+        <div class="projects-container">
+            <div class="container-projects">
+                <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <p>Front content goes here</p>
+                    </div>
+                    <div class="flip-card-back">
+                    <p> Back content goes here</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="container-projects">
+                <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <p> Front content goes here</p>
+                    </div>
+                    <div class="flip-card-back">
+                    <p> Back content goes here</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="container-projects">
+                <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <p> Front content goes here</p>
+                    </div>
+                    <div class="flip-card-back">
+                    <p> Back content goes here</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="container-projects">
+                <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    <p> Front content goes here</p>
+                    </div>
+                    <div class="flip-card-back">
+                    <p> Back content goes here</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-container">
+            <div class="login-container">
+                <h2 class="login-h2">Login</h2>
+                <form class="login-form">
+                <div class="form-group">
+                    <label class="login-label" for="username">Username:</label>
+                    <br>
+                    <input class="login-input" type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label class="login-label" for="password">Password:</label>
+                    <br>
+                    <input class="login-input" type="password" id="password" name="password" required>
+                </div>
+                <button class="login-button" type="submit">Login</button>
+                </form>
+            </div>
+            
+            <div class="contact-container">
+                <h2 class="contact-h2">Contact Me</h2>
+                <form class="contact-form">
+                <div class="form-group">
+                    <label class="contact-label" for="name">Name:</label>
+                    <br>
+                    <input class="contact-input" type="text" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label class="contact-label" for="email">Email:</label>
+                    <br>
+                    <input class="contact-input" type="email" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label class="contact-label" for="message">Message:</label>
+                    <br>
+                    <textarea class="contact-input" id="message" name="message" required></textarea>
+                </div>
+                <button class="contact-button" type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    </content>
+    
+    <footer>
+        <div class="footer-container">
+            <p class="footer-text" > Erstellt von Sibylle Gehring | HTML, CSS, JS, JQuery, PHP, Bootstrap</p>
+        </div>
+    </footer>
+                
+</body>
+</html>
